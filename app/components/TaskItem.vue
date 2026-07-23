17938a0872ce7abd0d2107eaed96d50c2892e87a
RafaL54 <karmann126@gmail.com>
2026-07-23 23:47:47 +0200

    Create task components and split todo logic

---
+++ app/components/TaskItem.vue
@@ -0,0 +1,78 @@
+<template>
+<div class="flex items-center justify-between w-full gap-3 border-b py-3"
+        >
+          <div class="flex items-center gap-2">
+            <input
+              type="checkbox"
+              :checked="task.completed"
+              class="accent-green-500 w-3 h-3"
+              @change="$emit('toggle', task)"
+            >
+
+            <input
+              v-if="editing"
+              v-model="editTitle"
+              class="border rounded px-2 py-1 w-full bg-white"
+              @keyup.enter="saveEdit(task)"
+              @blur="saveEdit(task)"
+              @keyup.esc="cancelEdit"
+            >
+
+            <span
+              v-else
+              :class="{
+                'line-through text-gray-400': task.completed
+              }"
+              @dblclick="edit(task)"
+            >
+              {{ task.title }}
+            </span>
+          </div>
+          <div v-if="task.completed">
+            <button
+              class="bg-red-500 text-white px-2 py-1 rounded"
+              @click="$emit('remove', task)"
+            >
+              Usuń
+            </button>
+          </div>
+		</div>
+</template>
+
+<script setup>
+
+const editing = ref(false)
+const editTitle = ref('')
+const cancelingEdit = ref(false)
+
+defineEmits([
+	'toggle', 
+	'remove'
+])
+
+defineProps({
+	task: Object	
+})
+
+function edit(task) {
+  editing.value = true
+  editTitle.value = task.title
+}
+
+function saveEdit(task) {
+
+  if (cancelingEdit.value) {
+    cancelingEdit.value = false
+    return
+  }
+  task.title = editTitle.value
+  editing.value = false
+}
+
+function cancelEdit() {
+
+  cancelingEdit.value = true
+  editing.value = false
+}
+
+</script>
---
+++ app/components/TaskList.vue
@@ -0,0 +1,32 @@
+<template>
+        <div
+          v-if="tasks.length === 0"
+          class="text-center text-gray-500 py-5"
+        >
+          Brak zadań
+        </div>
+
+        <div
+          v-for="task in tasks"
+           :key="task.id"
+          >
+		  <TaskItem 
+		   :task="task"
+		   @toggle="emit('toggle', $event)"
+		   @remove="emit('remove', $event)" 
+		  />
+		  </div>
+</template>
+
+<script setup>
+
+const emit = defineEmits([
+	'toggle',
+	'remove'
+])
+
+defineProps({
+	tasks: Array
+})
+
+</script>
