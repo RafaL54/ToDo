9fa7f41f63d68043ced6dd9ebae508e7f92534ac
RafaL54 <karmann126@gmail.com>
2026-08-03 12:43:12 +0200

    Add login page

---
+++ app/pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center p-6">
+    <div class="w-full max-w-md">
+      <div class="border rounded-lg p-6 shadow bg-gray-50">
+        <h1 class="text-2xl font-semibold mb-6">
+          Logowanie
+        </h1>
+
+        <UForm>
+          <UFormField label="E-mail">
+            <UInput />
+          </UFormField>
+
+          <UFormField label="Hasło">
+            <UInput type="password" />
+          </UFormField>
+
+          <UButton
+            type="submit"
+            class="mt-4"
+          >
+            Zaloguj
+          </UButton>
+        </UForm>
+      </div>
+    </div>
+  </div>
+</template>
